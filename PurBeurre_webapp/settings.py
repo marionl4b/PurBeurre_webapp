f868788a3d53c4244b396005d69a360e4fe0a9d2
marionl4b <marion.labarre@gmail.com>
2019-03-15 10:53:33 +0100

    Init starter bootstrap template

--- PurBeurre_webapp/settings.py
+++ PurBeurre_webapp/settings.py
@@ -120,5 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+INTERNAL_IPS = ['127.0.0.1']
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_URL = '/static/'
