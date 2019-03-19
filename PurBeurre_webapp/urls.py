c9a6bf24699282de723d2ded9812e42649902d9c
marionl4b <marion.labarre@gmail.com>
2019-03-19 10:50:48 +0100

    Init admin and create super user

--- PurBeurre_webapp/urls.py
+++ PurBeurre_webapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('manager/', admin.site.urls),
     path('', views.home, name='home')
 ]
