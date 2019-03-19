72c1d5e710b14fa4af6e6792247812d6cc957c94
marionl4b <marion.labarre@gmail.com>
2019-03-19 17:45:22 +0100

    Add test data to the database

--- website/models.py
+++ website/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -14,3 +18,7 @@ class Product(models.Model):
     nutriscore = models.CharField(max_length=1)
     nutrient_100g = models.TextField(null=True)
     categories = models.ManyToManyField(Category)
+    favorites = models.ManyToManyField(User, related_name="Favorite")
+
+    def __str__(self):
+        return self.name
