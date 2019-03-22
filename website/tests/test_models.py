f44aa8fda57ff623a4426c60214fd9ef54056867
marionl4b <marion.labarre@gmail.com>
2019-03-22 16:32:34 +0100

    Refactor test_models.py

--- website/tests/test_models.py
+++ website/tests/test_models.py
@@ -1,53 +1,33 @@
 import pytest
-from mixer.backend.django import mixer
 from website.models import *
-from django.contrib.auth.models import User
-
-
-@pytest.fixture
-def categories(db):
-    print("INIT CATEGORIES")
-    return (
-        mixer.blend(Category, name="Petit-déjeuners"),
-        mixer.blend(Category, name="Pâtes à tartiner"),
-        mixer.blend(Category, name="Sodas"),
-        mixer.blend(Category, name="Snacks")
-    )
-
-
-@pytest.fixture
-def products(db):
-    print("INIT PRODUCTS")
-    return (
-        mixer.blend(Product, name="Nutella", nutriscore="e"),
-        mixer.blend(Product, name="Coca-Cola", nutriscore="e"),
-        mixer.blend(Product, name="Snickers", nutrsiscore="e"),
-        mixer.blend(Product, name="Coca-Cola zéro", nutrsicore="b")
-    )
-
-
-@pytest.fixture
-def users(db):
-    print("INIT USERS")
-    return mixer.cycle(3).blend(User)
-
 
 @pytest.mark.django_db
 class TestModels:
+    @pytest.fixture
+    def setup(self):
+        """ Init test data from models in test database """
+        print("INIT DATA")
 
-    def test_created_categories(self, categories):
-        assert Category.objects.count() == 4
+        # categories
+        self.breakfast = Category.objects.create(name="breakfast")
+        self.snacks = Category.objects.create(name="snacks")
+        self.chocolate = Category.objects.create(name="chocolate")
 
-    def test_created_products(self, products):
-        assert Product.objects.count() == 4
+        # products
+        self.snicker = Product.objects.create(name="snicker", nutriscore="e")
+        self.nutella = Product.objects.create(name="nutella", nutriscore="e")
 
-    def test_created_users(self, users):
-        assert User.objects.count() == 3
+        # pruduct-categories
+        self.snicker.categories.add(self.snacks)
+        self.nutella.categories.add(self.breakfast, self.chocolate)
 
-    def test_category_is_breackfast(self, categories):
-        category_breakfast = Category.objects.get(name="Petit-déjeuners")
-        assert category_breakfast.name == "Petit-déjeuners"
+    def test_product_has_category(self, setup):
+        """ GIVEN : tests data for Category and Product
+            THEN : retreive category name for snickers product
+            WHEN : many-to-many relation has been set between product and category """
+        snicker_has_cat = self.snicker.categories.values("name")
+        assert list(snicker_has_cat) == [{'name': 'snacks'}]
 
-    def test_product_nutrsicore_is_e(self, products):
-        nutella = Product.objects.get(name="Nutella")
-        assert nutella.nutriscore == "e"
+    def test_product_has_multiple_category(self, setup):
+        nutella_categories = self.nutella.categories.values_list("name")
+        assert list(nutella_categories) == [("breakfast",), ("chocolate",)]
