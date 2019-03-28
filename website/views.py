dac7ccf55d9f1959faef2c393c86d8946efed100
marionl4b <marion.labarre@gmail.com>
2019-03-28 14:40:10 +0100

    Feature - Add grammes values for nutrient in website.detail view

--- website/views.py
+++ website/views.py
@@ -13,6 +13,28 @@ def result(request):
 
 
 def detail(request, product_id):
+    """ WHEN: product_id in database
+        THEN: show product details and construct a dictionary for nutrients """
     product = get_object_or_404(Product, pk=product_id)
     context = {'product': product}
+    if product.nutrient_100g != "":
+        nutrient_100g = sorted(product.nutrient_100g.split(","))
+        nutrient_list = []
+        if len(nutrient_100g) > 0:
+            i = -1
+            for n in nutrient_100g:
+                i += 1
+                n = nutrient_100g[i].split(":")
+                nutrient_list.append(n)
+            nutrients = {
+                'fat': nutrient_list[0][1],
+                'saturated_fat': nutrient_list[1][1],
+                'sugars': nutrient_list[2][1],
+                'salt': nutrient_list[3][1],
+                'fat_level': nutrient_list[0][2],
+                'saturated_fat_level': nutrient_list[1][2],
+                'sugars_level': nutrient_list[2][2],
+                'salt_level': nutrient_list[3][2],
+            }
+            context = {'product': product, 'nutrients': nutrients}
     return render(request, 'website/detail_product.html', context)
