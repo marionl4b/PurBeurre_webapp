82939cf4c44dfc6d30f969c5afae5df2bc65aab7
marionl4b <marion.labarre@gmail.com>
2019-04-11 10:02:31 +0200

    Fix - Heroku databases settings

--- PurBeurre_webapp/settings.py
+++ PurBeurre_webapp/settings.py
@@ -139,7 +139,8 @@ STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+if os.environ.get('ENV') == 'PRODUCTION':
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Activate Pytest for ./manage.py test
 TEST_RUNNER = 'pytest_runner.PytestTestRunner'
